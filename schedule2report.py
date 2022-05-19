9eaf3d6225c6b2e4aca15fd1cd5a85985795ce7b
Dan Gauerke <89668769+colliercoder@users.noreply.github.com>
2022-05-19 08:47:11 -0500

    file for work

--- schedule2report.py
+++ schedule2report.py
@@ -1,9 +1,4 @@
-from calendar import weekday
-
-import pandas as pd
 import xlwings as xw
-import matplotlib.pyplot as plt
-import datetime
 import openpyxl
 from openpyxl.utils import get_column_letter, column_index_from_string
 
@@ -14,9 +9,13 @@ wb = openpyxl.load_workbook(r"C:\Users\15402\Desktop\python_hours_script\miner_s
 
 #Instantiate a sheet object
 june_schedule = schedule.sheets['MUZO EMPLOYEES JUNE']
-extra_horas = schedule.sheets['HORA EXTRA']
+extra_horas = hours_report.sheets['HORA EXTRA']
+domingo_festivo = hours_report.sheets['DOMINGO Y FESTIVO']
+recargo_nocturno = hours_report.sheets['RECARGO NOCTURNO']
+
+
 sheet = wb['MUZO EMPLOYEES JUNE']
-lista = schedule.sheets['LISTA']
+lista = hours_report.sheets['LISTA']
 
 #Variables for novedad o tipo de hora 'G'
 diurno=lista.range('C1').value
@@ -47,7 +46,12 @@ columnD = 'D' #names column
 columnC = 'C' #cedula column
 
 count = 0
+count2 = 0
+count3 = 0
 bigdict = {}
+domingo_dict = {}
+nocturno_dict = {}
+
 for i in range(8,sheet.max_row+1): #looping down names
     name = columnD + str(i)
     cedula = columnC + str(i)
@@ -64,16 +68,21 @@ for i in range(8,sheet.max_row+1): #looping down names
             dicts={'name':name,'cedula':cedula,'date':date,'shift':shift}
             count+=1
 
-            #keys = ['name','cedula','date','shift']
-            #values = [name,cedula,date,shift]
-            #dicts[keys['name']] = values[name]
-            #print(dicts,count)
-            bigdict[count]=dicts
-            #print(count)
+            bigdict[count] = dicts
 
-            #print(name,cedula,date,shift)
-#print(bigdict)
-#print("LENGTH OF BIGDICT",str(len(bigdict)))
+        if shift != 'O' and name != 'NEW PERSON' and date.weekday() == 6:
+            doms = {'name': name, 'cedula': cedula, 'date': date, 'shift': shift}
+            count2 += 1
+
+            domingo_dict[count2] = doms
+
+        if shift == 'N' and name != 'NEW PERSON':
+            nocs = {'name': name, 'cedula': cedula, 'date': date, 'shift': shift}
+            count3 +=1
+
+            nocturno_dict[count3] = nocs
+
+
 
 columnC = 'C' #documento,----- cedula
 columnD = 'D' #nombre apellido,----- name
@@ -105,6 +114,7 @@ for entry in range(len(bigdict)):
     extra_horas.range(cellL).value = bigdict[entry + 1]['date']
 
     if bigdict[entry+1]['date'].weekday() == 6 and bigdict[entry+1]['shift'] == 'N':
+        #HORA EXTRA SHEET
         extra_horas.range(cellG).value = domN
         extra_horas.range(cellH).value = night_turno_hrs
         extra_horas.range(cellI).value = C_turno_domingo
@@ -112,6 +122,7 @@ for entry in range(len(bigdict)):
         extra_horas.range(cellK).value = actividad
         extra_horas.range(cellM).value = num_hours
     elif bigdict[entry+1]['date'].weekday() == 6 and bigdict[entry+1]['shift'] == 'D':
+        # HORA EXTRA SHEET
         extra_horas.range(cellG).value = domD
         extra_horas.range(cellH).value = day_turno_hrs
         extra_horas.range(cellI).value = A_turno_domingo
@@ -119,6 +130,7 @@ for entry in range(len(bigdict)):
         extra_horas.range(cellK).value = actividad
         extra_horas.range(cellM).value = num_hours
     elif bigdict[entry+1]['date'].weekday() != 6 and bigdict[entry+1]['shift'] == 'N':
+        # HORA EXTRA SHEET
         extra_horas.range(cellG).value = nocturno
         extra_horas.range(cellH).value = night_turno_hrs
         extra_horas.range(cellI).value = C_turno
@@ -126,6 +138,7 @@ for entry in range(len(bigdict)):
         extra_horas.range(cellK).value = actividad
         extra_horas.range(cellM).value = num_hours
     else:
+        # HORA EXTRA SHEET
         extra_horas.range(cellG).value = diurno
         extra_horas.range(cellH).value = day_turno_hrs
         extra_horas.range(cellI).value = A_turno
@@ -133,11 +146,75 @@ for entry in range(len(bigdict)):
         extra_horas.range(cellK).value = actividad
         extra_horas.range(cellM).value = num_hours
 
+
+#DOMINGO Y FESTIVO
+for entry in range(len(domingo_dict)):
+    cellC = columnC + str(entry + row)
+    cellD = columnD + str(entry + row)
+    cellG = columnG + str(entry + row)
+    cellH = columnH + str(entry + row)
+    cellI = columnI + str(entry + row)
+    cellJ = columnJ + str(entry + row)
+    cellK = columnK + str(entry + row)
+    cellL = columnL + str(entry + row)
+    cellM = columnM + str(entry + row)
+
+    if domingo_dict[entry + 1]['shift'] == 'N':
+        # DOMINGO Y FESTIVO SHEET
+        domingo_festivo.range(cellC).value = domingo_dict[entry + 1]['cedula']
+        domingo_festivo.range(cellD).value = domingo_dict[entry + 1]['name']
+        domingo_festivo.range(cellI).value = domingo_dict[entry + 1]['date']
+        domingo_festivo.range(cellJ).value = 10
+        domingo_festivo.range(cellG).value = 'DOMINGO'
+        domingo_festivo.range(cellH).value = 'DOMINGO C'
+    else:
+        # DOMINGO Y FESTIVO SHEET
+        domingo_festivo.range(cellC).value = domingo_dict[entry + 1]['cedula']
+        domingo_festivo.range(cellD).value = domingo_dict[entry + 1]['name']
+        domingo_festivo.range(cellI).value = domingo_dict[entry + 1]['date']
+        domingo_festivo.range(cellJ).value = 10
+        domingo_festivo.range(cellG).value = 'DOMINGO'
+        domingo_festivo.range(cellH).value = 'DOMINGO A'
+
+
+#RECARGO NOCTURNO
+
+recargo_tiempo = '09:00PM'
+recargo_horas = 7
+for entry in range(len(nocturno_dict)):
+    cellC = columnC + str(entry+row)
+    cellD = columnD + str(entry+row)
+    cellG = columnG + str(entry+row)
+    cellH = columnH + str(entry+row)
+    cellI = columnI + str(entry+row)
+    cellJ = columnJ + str(entry+row)
+    cellK = columnK + str(entry+row)
+    cellL = columnL + str(entry + row)
+    cellM = columnM + str(entry+row)
+
+    if nocturno_dict[entry+1]['date'].weekday() == 6 and nocturno_dict[entry+1]['shift'] == 'N':
+        #HORA EXTRA SHEET
+        recargo_nocturno.range(cellC).value = nocturno_dict[entry + 1]['cedula']
+        recargo_nocturno.range(cellD).value = nocturno_dict[entry + 1]['name']
+        recargo_nocturno.range(cellH).value = nocturno_dict[entry + 1]['date']
+
+        recargo_nocturno.range(cellG).value = 'NOCTURNO DOMINICAL O FESTIVO'
+
+        recargo_nocturno.range(cellI).value = recargo_tiempo
+        recargo_nocturno.range(cellJ).value = recargo_horas
+
+    else:
+        # HORA EXTRA SHEET
+        recargo_nocturno.range(cellC).value = nocturno_dict[entry + 1]['cedula']
+        recargo_nocturno.range(cellD).value = nocturno_dict[entry + 1]['name']
+        recargo_nocturno.range(cellH).value = nocturno_dict[entry + 1]['date']
+
+        recargo_nocturno.range(cellG).value = 'NOCTURNO'
+
+        recargo_nocturno.range(cellI).value = recargo_tiempo
+        recargo_nocturno.range(cellJ).value = recargo_horas
+
 print("complete")
-
-
-
-
 
 
 
